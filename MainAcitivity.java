62d630f34bbfbb365812e3fa55f2cd4c2a42a3ec
Lavish Garg <53224426+Lavish171@users.noreply.github.com>
2020-03-03 23:44:46 +0530

    Create MainAcitivity.java
    This code is for the list making and for the first screen where location is to be saved in the list

---
+++ MainAcitivity.java
@@ -0,0 +1,38 @@
+package com.example.elavi.googlemapsfavouriteplaces;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    static ArrayList<String> places=new ArrayList<>();
+    static ArrayList<LatLng> locations=new ArrayList<>();
+    static  ArrayAdapter arrayAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView=findViewById(R.id.listview);
+        places.add("Add the new place");
+       locations.add(new LatLng(0,0));
+        arrayAdapter=new ArrayAdapter(getApplicationContext(),android.R.layout.simple_list_item_1,places);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent=new Intent(getApplicationContext(),MapsActivity.class);
+                intent.putExtra("Placename",i);
+                startActivity(intent);
+            }
+        });
+    }
+}
